daa655684e399cc432f0e1e09cdcade40d5f4c7f
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-07-20 22:26:23 +0530

    Initialisation

---
+++ InSearchOfAnEasyProblem.java
@@ -0,0 +1,25 @@
+                        // 1030A - In Search Of an Easy Problem //
+
+package Solutions;
+import java.util.*;
+
+public class InSearchOfAnEasyProblem {
+    public static void main (String []args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            int answer = sc.nextInt();
+            
+            if (answer == 1) {
+                System.out.println("HARD");
+                sc.close();
+                return;
+            }
+        }
+        System.out.println("EASY");
+        sc.close();
+    }
+}
