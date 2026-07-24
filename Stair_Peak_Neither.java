97fa26bef48349e9fbedf9df02ab824ca085dd06
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-07-24 20:52:57 +0530

    Day 7

---
+++ Stair_Peak_Neither.java
@@ -0,0 +1,28 @@
+                                                    // 1950A - Stair, Peak or Neither? //
+
+import java.util.*;
+
+public class Stair_Peak_Neither {
+    public static void main(String []args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            if (a < b && b < c) {
+                System.out.println("STAIR");
+            }
+            else if (b > a && b > c) {
+                System.out.println("PEAK");
+            }
+            else {
+                System.out.println("NONE");
+            }
+        }
+        sc.close();
+    }
+}
