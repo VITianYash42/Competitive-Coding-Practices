d6aa3d5fee3cfe6ab9d3f0edba243fa3c4abd1c4
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2026-07-28 18:17:43 +0530

    Day 11

---
+++ ToMyCritics.java
@@ -0,0 +1,26 @@
+                                                     // 1850A - To My Critics //
+
+import java.util.*;
+
+public class ToMyCritics {
+    public static void main(String []args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        for (int i = 1; i<= t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            if (a + b >= 10 || a + c >= 10 || b + c >= 10) {
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
